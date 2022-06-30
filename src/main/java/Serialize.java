15fa6f422138939d996ea9dd7ce978a4a27ab18b
Hocine <ocineh@hotmail.com>
2022-06-30 18:25:45 +0200

    Add a subcommand to export and import tasks in json format

--- src/main/java/Serialize.java
+++ src/main/java/Serialize.java
@@ -1,4 +1,9 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.Base64;
 
 public interface Serialize extends java.io.Serializable {
@@ -15,5 +20,19 @@ public interface Serialize extends java.io.Serializable {
         oos.writeObject(obj);
         oos.close();
         return Base64.getEncoder().encodeToString(stream.toByteArray());
+    }
+
+    static <T extends java.io.Serializable> String toJson(T obj, boolean pretty) {
+        GsonBuilder builder = new GsonBuilder();
+        if(pretty) builder.setPrettyPrinting();
+        return builder.create().toJson(obj);
+    }
+
+    static <T extends java.io.Serializable> T fromJson(JsonReader json, Type type) {
+        return new Gson().fromJson(json, type);
+    }
+
+    static <T extends java.io.Serializable> T fromJson(JsonReader json, Class<T> clazz) {
+        return new Gson().fromJson(json, clazz);
     }
 }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,11 +1,13 @@
+import com.google.gson.stream.JsonReader;
 import picocli.CommandLine;
 import picocli.CommandLine.*;
 import picocli.CommandLine.Model.CommandSpec;
 
+import java.io.*;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
-@Command(name = "tocli", mixinStandardHelpOptions = true, version = "tocli 0.1.1")
+@Command(name = "tocli", mixinStandardHelpOptions = true, version = "tocli 0.1.2")
 public class Todo implements Callable<Integer> {
     private final TaskList tasks = new TaskList();
     @Option(
@@ -77,13 +79,75 @@ public class Todo implements Callable<Integer> {
     }
 
     @Command(
-            name = "undone", description = "Mark a task as undone", mixinStandardHelpOptions = true
+            name = "undone",
+            description = "Mark a task as undone",
+            mixinStandardHelpOptions = true
     )
     public void undone(@Parameters(paramLabel = "<ID>", description = "The ID of the task") int id)
     throws ParameterException {
         Task task = tasks.get(id);
         if(task != null) task.undone();
         else throw new ParameterException(spec.commandLine(), "No task exists with this id.");
+    }
+
+    @Command(
+            name = "export",
+            description = "Export the tasks to a file (if specified) in JSON format",
+            mixinStandardHelpOptions = true
+    )
+    public void exportTasks(
+            @Option(
+                    names = {"-o", "--output"},
+                    description = "The path of the file where to export the tasks (default: in the terminal)"
+            ) Path file,
+            @Option(
+                    names = {"--pretty", "-p"},
+                    description = "Pretty print the JSON file (default: ${DEFAULT-VALUE})"
+            ) boolean pretty
+    ) {
+        Writer writer;
+        if(file == null) writer = new OutputStreamWriter(System.out);
+        else {
+            try {
+                writer = new FileWriter(file.toFile());
+            } catch(IOException ignored) {
+                throw new ParameterException(spec.commandLine(), "Could not open output file.");
+            }
+        }
+
+        try {
+            String json = Serialize.toJson(tasks, pretty);
+            writer.write(json);
+            writer.flush();
+            writer.close();
+        } catch(IOException e) {
+            throw new ParameterException(
+                    spec.commandLine(),
+                    "Error while writing into the output file."
+            );
+        }
+    }
+
+    @Command(
+            name = "import",
+            description = "Import tasks from a file in JSON format",
+            mixinStandardHelpOptions = true
+    )
+    public void importTasks(
+            @Parameters(
+                    paramLabel = "<INPUT>",
+                    description = "The path of the file from where to import the tasks"
+            ) Path path
+    ) {
+        try {
+            JsonReader reader = new JsonReader(new FileReader(path.toFile()));
+            for(Task task: Serialize.fromJson(reader, TaskList.class))
+                tasks.add(task);
+        } catch(FileNotFoundException e) {
+            throw new ParameterException(spec.commandLine(), "The input file does not exist.");
+        } finally {
+            tasks.save(data);
+        }
     }
 
     @Override
