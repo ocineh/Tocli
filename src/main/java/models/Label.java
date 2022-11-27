dadfb5645264e24eee826cd4f1470fb05635f875
Hocine <ocineh@hotmail.com>
2022-11-27 04:31:35 +0100

    Add the Label class

---
+++ src/main/java/models/Label.java
@@ -0,0 +1,13 @@
+package models;
+
+public final class Label {
+	private final String name;
+
+	public Label(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
